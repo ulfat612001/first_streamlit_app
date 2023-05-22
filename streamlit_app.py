b7047fa4bdd581bca2611cfd36d9b5dd27d9290f
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-22 13:25:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
